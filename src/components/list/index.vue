8ea15c39a778394c7ca0e6f4b20bfd09746f93ca
InsHomePgup <7328234@gmail.com>
2024-02-06 13:16:28 +0800

    feat: list component

---
+++ src/components/list/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <!-- <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list> -->
+  </div>
+</template>
+
+<style scoped></style>
